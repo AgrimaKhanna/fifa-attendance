ebbfe33529bd17059109b77694aaf10d8fdc5ca8
AgrimaKhanna <123540290+AgrimaKhanna@users.noreply.github.com>
2023-01-25 14:15:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ df["Attendance"] = df["Attendance"].replace({r'[^0-9.]+': ''}, regex=True)
 # Convert the Attendance column from object to numeric datatype
 df['Attendance'] = pd.to_numeric(df['Attendance'], errors='coerce')
 
-# Group by unique vales for Home country and add the number of attendees
+# Group by unique vales for Home country and aggregate of the number of attendees for each country
 Home_attendance = df.groupby(['Home'])['Attendance'].sum().reset_index(name='Attendance')
+
+# Rename columns of Home_attendance
 Home_attendance.columns = ['Country', 'H_Attend']
 
-
-# Group by unique vales for Away country and add the number of attendees
+# Group by unique vales for Away country and aggregate of the number of attendees for each country.
 Away_attendance = df.groupby(['Away'])['Attendance'].sum().reset_index(name='Attendance')
-Away_attendance.columns = ['Country', 'A_Attend']
 
+# Rename columns of Away_attendance
+Away_attendance.columns = ['Country', 'A_Attend']
 
 merged_df = pd.merge(Home_attendance, Away_attendance, on='Country')
 merged_df['Total_Attendance'] = merged_df['H_Attend'] + merged_df['A_Attend']
